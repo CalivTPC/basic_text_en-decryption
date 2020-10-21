d80e5b858fa7055b1bca40cf6e63942e58504f4d
Timeo <work@timeo.li>
2020-10-21 15:59:29 +0200

    requestSession() now fully working + some simple improvements for restoreData() & save()

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from pprint import pprint
 
 dictApp = {
     "sessionSettings": {
-        "mode": "S"
+        "mode": "S",
+        "session": ""
     },
     "defaultSettings": {
         "mode": "S",
+        "session": ""
     },
     "storedAnswers":
         {"yes": "Y",
@@ -13,7 +15,14 @@ dictApp = {
          "default": "D",
          "cancel": "C",
          "explainMode": "E",
-         "simpleMode": "S"},
+         "simpleMode": "S",
+         "1": "1",
+         "2": "2",
+         "3": "3",
+         "4": "4",
+         "5": "5",
+         "continue": ""
+        },
 
     "StoredNames": {
         "mode": {
@@ -21,47 +30,64 @@ dictApp = {
             "S": "Simple Mode",
         },
     },
+    "seesions": {
+        "names": [],
+        "publicKeys": [],
+        "privateKeys": [],
+        "sessionKeys": [],
+    }
 }
 
 
 texts = {
     "questions": {"mode": "Wollen sie den " + dictApp["StoredNames"]["mode"]["S"] + "(" + dictApp["storedAnswers"]
-    ["explainMode"] + ") nutzen oder wollen sie den " + dictApp["StoredNames"]["mode"]["E"] + "(" + dictApp["storedAnswers"]
-    ["simpleMode"] + ") Nutzen: "},
+                            ["simpleMode"] + ") nutzen oder wollen sie den " + dictApp["StoredNames"]["mode"]["E"] + "(" + dictApp["storedAnswers"]
+                            ["explainMode"] + ") Nutzen: ",
+                  "S": {
+                        "session": "Welche session wollen Sie nutzen? " + "(" + dictApp["storedAnswers"]["1"] + ")" + " (" + dictApp["storedAnswers"]["2"] + ")" + " (" + dictApp["storedAnswers"]["3"] + ")" + " (" + dictApp["storedAnswers"]["4"] + ")" + " (" + dictApp["storedAnswers"]["5"] + "): "
+                      },
+                  "E": {
+                        "session": "Sie können später Sessions speichern. Was soviel heisst wie sie können den privat/public/session Key speichern \n"
+                                   "Welche session wollen Sie nutzen? " + "(" + dictApp["storedAnswers"]["1"] + ")" + " (" + dictApp["storedAnswers"]["2"] + ")" + " (" + dictApp["storedAnswers"]["3"] + ")" + " (" + dictApp["storedAnswers"]["4"] + ")" + " (" + dictApp["storedAnswers"]["5"] + "): "
+                  },
+                  },
 
     "answerOptions": {"mode": [
         dictApp["storedAnswers"]["explainMode"], dictApp["storedAnswers"]["simpleMode"], dictApp["storedAnswers"]["default"]
-        ]}
+        ],
 
+        "sessions": [
+            dictApp["storedAnswers"]["1"], dictApp["storedAnswers"]["2"], dictApp["storedAnswers"]["3"], dictApp["storedAnswers"]["4"], dictApp["storedAnswers"]["5"], dictApp["storedAnswers"]["continue"]
+    ]}
 }
 
 def start():
-    restoreData()
+    #restoreData()
     requestMode()
-
-    save()
+    requestSession()
+    save("file")
+    log()
 
 
 def restoreData():
-    dictAppTxt = open("dictApp.txt")
-    global dictApp
-    dictApp = eval(dictAppTxt.read())
-    dictAppTxt.close()
-
-    textsTxt = open("texts.txt")
-    global texts
-    texts = eval(textsTxt.read())
-    textsTxt.close()
+    global dictApp, texts
+    values = ["dictApp", "texts"]
+    for i in range(len(values)):
+        file = open(values[i] + ".txt")
+        values[i] = eval(file.read())
+        file.close()
 
 
-def save():
-    dictAppTxt = open("dictApp.txt", "w")
-    dictAppTxt.write(str(dictApp))
-    dictAppTxt.close()
+def save(type):
+    values = ["dictApp", "texts"]
+    if type == "file":
+        for i in range(len(values)):
+            file = open(values[i] + ".txt", "w")
+            file.write(str(values[i]))
+            file.close()
 
-    textsTxt = open("texts.txt", "w")
-    textsTxt.write(str(texts))
-    textsTxt.close()
+    elif type == "session":
+        print("something")
 
 
 def basicRequest(text, answerOptions):
@@ -85,6 +111,39 @@ def requestMode():
     else:
         dictApp["sessionSettings"]["mode"] = dictApp["defaultSettings"]["mode"]
         print("Sie nutzen nun den " + dictApp["StoredNames"]["mode"][dictApp["defaultSettings"]["mode"]])
+
+
+def requestSession():
+    answer = basicRequest(texts["questions"][dictApp["sessionSettings"]["mode"]]["session"], texts["answerOptions"]["sessions"])
+    if answer == dictApp["storedAnswers"]["1"]:
+        print("answer: " + answer)
+        dictApp["defaultSettings"]["session"] = answer
+        dictApp["sessionSettings"]["session"] = answer
+
+    elif answer == dictApp["storedAnswers"]["2"]:
+        print("answer: " + answer)
+        dictApp["defaultSettings"]["session"] = answer
+        dictApp["sessionSettings"]["session"] = answer
+
+    elif answer == dictApp["storedAnswers"]["3"]:
+        print("answer: " + answer)
+        dictApp["defaultSettings"]["session"] = answer
+        dictApp["sessionSettings"]["session"] = answer
+
+    elif answer == dictApp["storedAnswers"]["4"]:
+        print("answer: " + answer)
+        dictApp["defaultSettings"]["session"] = answer
+        dictApp["sessionSettings"]["session"] = answer
+
+    elif answer == dictApp["storedAnswers"]["5"]:
+        print("answer: " + answer)
+        dictApp["defaultSettings"]["session"] = answer
+        dictApp["sessionSettings"]["session"] = answer
+
+    else:
+        print("answer: " + answer)
+        dictApp["defaultSettings"]["session"] = answer
+        dictApp["sessionSettings"]["session"] = answer
 
 
 def log():
